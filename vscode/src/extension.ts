8eb34d38e7b284d0570f817a876fb478aa436db2
Qingqing Sun <qisun@microsoft.com>
2018-04-26 15:10:41 +0800

    check java home and java versions with npm packages

--- vscode/src/extension.ts
+++ vscode/src/extension.ts
@@ -4,6 +4,7 @@ import * as path from "path";
 import { commands, ExtensionContext, Uri, window, workspace } from "vscode";
 import * as xml2js from "xml2js";
 import { Services } from "./services";
+import { Utility } from "./utility";
 import { ProjectExplorer } from "./views/projectExplorer";
 
 export function activate(context: ExtensionContext) {
@@ -13,38 +14,33 @@ export function activate(context: ExtensionContext) {
 }
 
 async function createJavaProject(extensionPath: string) {
-    const versionFileMapping: Map<string, string> = new Map([["jdk1.8", "Java8"], ["jdk-9", "Java9"], ["jdk-10", "Java10"]]);
-    const javaHome: string = process.env.JAVA_HOME;
-    if (!javaHome) {
-        window.showErrorMessage("Please install JDK and set JAVA_HOME in environment variables first!");
+    let javaVersion: number;
+    try {
+        const javaHome: string = await Utility.checkJavaRuntime();
+        javaVersion = await Utility.checkJavaVersion(javaHome);
+    } catch (error) {
+        window.showErrorMessage(error.message, error.label).then((selection) => {
+            if (error.label && error.label === selection && error.openUrl) {
+                commands.executeCommand("vscode.open", error.openUrl);
+            }
+        });
         return;
     }
-    let matchedValue: string;
-    for (const key of versionFileMapping.keys()) {
-        if (javaHome.match(key) !== null && javaHome.match(key).length !== 0) {
-            matchedValue = javaHome.match(key)[0];
-        }
-    }
-    if (!matchedValue) {
-        window.showErrorMessage("We currently only support JDK (version 1.8.0 or later).");
-        return;
-    }
-    const targetResourceFolder = path.join(extensionPath, "resources", versionFileMapping.get(matchedValue));
     const location: Uri[] = await window.showOpenDialog({
         defaultUri: workspace.rootPath ? Uri.file(workspace.rootPath) : undefined,
         canSelectFiles: false,
         canSelectFolders: true,
         openLabel: "Select the location",
     });
-    if (!location) {
+    if (!location || !location.length) {
         return;
     }
     const basePath: string = location[0].fsPath;
     const projectName: string = await window.showInputBox({
-        prompt: "input java project name",
+        prompt: "Input a java project name",
         validateInput: (name: string): string => {
             if (name && !name.match(/^[^*~/\\]+$/)) {
-                return "please input a valid project name";
+                return "Please input a valid project name";
             }
             if (name && fse.pathExistsSync(path.join(basePath, name))) {
                 return "A project with this name already exists.";
@@ -57,34 +53,19 @@ async function createJavaProject(extensionPath: string) {
     }
     const projectFile: string = path.join(basePath, projectName, ".project");
     await Promise.all([
-        fse.copy(path.join(extensionPath, "resources", "App.java.sample"),
-            path.join(basePath, projectName, "src", "app", "App.java")),
-        fse.copy(path.join(targetResourceFolder, "org.eclipse.jdt.core.prefs"),
-            path.join(basePath, projectName, ".settings", "org.eclipse.jdt.core.prefs")),
-        fse.copy(path.join(targetResourceFolder, ".classpath"),
-            path.join(basePath, projectName, ".classpath")),
-        fse.copy(path.join(extensionPath, "resources", ".project"), projectFile),
+        fse.copy(path.join(extensionPath, "templates", "App.java.sample"), path.join(basePath, projectName, "src", "app", "App.java")),
+        fse.copy(path.join(extensionPath, "templates", `Java${javaVersion}`), path.join(basePath, projectName)),
+        fse.copy(path.join(extensionPath, "templates", ".project"), projectFile),
         fse.ensureDir(path.join(basePath, projectName, "bin")),
     ]);
 
     // replace the project name with user input project name
     const xml: string = await fse.readFile(projectFile, "utf8");
-    const jsonObj: any = await parseXml(xml);
+    const jsonObj: any = await Utility.parseXml(xml);
     jsonObj.projectDescription.name = projectName;
     const builder: xml2js.Builder = new xml2js.Builder();
     const newXml: string = builder.buildObject(jsonObj);
     await fse.writeFile(projectFile, newXml);
 
     commands.executeCommand("vscode.openFolder", Uri.file(path.join(basePath, projectName)), true);
-}
-
-async function parseXml(xml: string): Promise<any> {
-    return new Promise((resolve: (obj: {}) => void, reject: (e: Error) => void): void => {
-        xml2js.parseString(xml, { explicitArray: true }, (err: Error, res: {}) => {
-            if (err) {
-                return reject(err);
-            }
-            return resolve(res);
-        });
-    });
 }
---
+++ vscode/src/utility.ts
@@ -0,0 +1,107 @@
+import * as child_process from "child_process";
+import * as findJavaHome from "find-java-home";
+import * as fse from "fs-extra";
+import * as path from "path";
+import { Uri, workspace } from "vscode";
+import * as xml2js from "xml2js";
+
+export class Utility {
+    public static async parseXml(xml: string): Promise<any> {
+        return new Promise((resolve: (obj: {}) => void, reject: (e: Error) => void): void => {
+            xml2js.parseString(xml, { explicitArray: true }, (err: Error, res: {}) => {
+                if (err) {
+                    return reject(err);
+                }
+                return resolve(res);
+            });
+        });
+    }
+
+    public static checkJavaVersion(javaHome: string): Promise<number> {
+        return new Promise((resolve, reject) => {
+            child_process.execFile(javaHome + "/bin/java", ["-version"], {}, (error, stdout, stderr) => {
+                const javaVersion: number = this.parseMajorVersion(stderr);
+                if (javaVersion < 8) {
+                    this.openJDKDownload(reject, "Java 8 or more recent is required to run. Please download and install a recent JDK");
+                } else {
+                    resolve(javaVersion);
+                }
+            });
+        });
+    }
+    public static checkJavaRuntime(): Promise<string> {
+        const isWindows = process.platform.indexOf("win") === 0;
+        const JAVAC_FILENAME = "javac" + (isWindows ? ".exe" : "");
+        return new Promise(async (resolve, reject) => {
+            let source: string;
+            let javaHome: string = this.readJavaConfig();
+            if (javaHome) {
+                source = "The java.home variable defined in VS Code settings";
+            } else {
+                javaHome = process.env.JDK_HOME;
+                if (javaHome) {
+                    source = "The JDK_HOME environment variable";
+                } else {
+                    javaHome = process.env.JAVA_HOME;
+                    source = "The JAVA_HOME environment variable";
+                }
+            }
+            if (javaHome) {
+                if (!await fse.pathExists(javaHome)) {
+                    this.openJDKDownload(reject, source + " points to a missing folder");
+                }
+                if (!fse.pathExists(path.resolve(javaHome, "bin", JAVAC_FILENAME))) {
+                    this.openJDKDownload(reject, source + " does not point to a JDK.");
+                }
+                return resolve(javaHome);
+            }
+            // No settings, let"s try to detect as last resort.
+            findJavaHome((err, home) => {
+                if (err) {
+                    this.openJDKDownload(reject, "Java runtime could not be located");
+                } else {
+                    resolve(home);
+                }
+            });
+        });
+    }
+    private static openJDKDownload(reject, cause) {
+        let jdkUrl = "http://developers.redhat.com/products/openjdk/overview/?from=vscode";
+        if (process.platform === "darwin") {
+            jdkUrl = "http://www.oracle.com/technetwork/java/javase/downloads/index.html";
+        }
+        reject({
+            message: cause,
+            label: "Get Java Development Kit",
+            openUrl: Uri.parse(jdkUrl),
+            replaceClose: false,
+        });
+    }
+
+    private static parseMajorVersion(content: string) {
+        let regexp = /version "(.*)"/g;
+        let match = regexp.exec(content);
+        if (!match) {
+            return 0;
+        }
+        let version = match[1];
+        // Ignore "1." prefix for legacy Java versions
+        if (version.startsWith("1.")) {
+            version = version.substring(2);
+        }
+        // look into the interesting bits now
+        regexp = /\d+/g;
+        match = regexp.exec(version);
+        let javaVersion = 0;
+        if (match) {
+            // tslint:disable-next-line:radix
+            javaVersion = parseInt(match[0]);
+        }
+        return javaVersion;
+    }
+
+    private static readJavaConfig() {
+        const config = workspace.getConfiguration();
+        return config.get<string>("java.home", null);
+    }
+}
