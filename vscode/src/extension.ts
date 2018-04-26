1baaa480280277a489246a19497bbd260053319e
Qingqing Sun <qisun@microsoft.com>
2018-04-26 11:49:40 +0800

    rename packageExplorer to projectExplorer, move createProject function in extension

--- vscode/src/extension.ts
+++ vscode/src/extension.ts
@@ -1,12 +1,90 @@
 
-import { commands, ExtensionContext, window } from "vscode";
+import * as fse from "fs-extra";
+import * as path from "path";
+import { commands, ExtensionContext, Uri, window, workspace } from "vscode";
+import * as xml2js from "xml2js";
 import { Services } from "./services";
-import { PackageExplorer } from "./views/packageExplorer";
 import { ProjectExplorer } from "./views/projectExplorer";
 
 export function activate(context: ExtensionContext) {
     Services.initialize(context);
-    const projectExplorer: ProjectExplorer = new ProjectExplorer(context);
-    context.subscriptions.push(window.registerTreeDataProvider("javaProjectExplorer", new PackageExplorer(context)));
-    context.subscriptions.push(commands.registerCommand("java.project.create", () => { projectExplorer.createJavaProject(); }));
+    context.subscriptions.push(window.registerTreeDataProvider("javaProjectExplorer", new ProjectExplorer(context)));
+    context.subscriptions.push(commands.registerCommand("java.project.create", () => { createJavaProject(context.extensionPath); }));
+}
+
+async function createJavaProject(extensionPath: string) {
+    const versionFileMapping: Map<string, string> = new Map([["jdk1.8", "Java8"], ["jdk-9", "Java9"], ["jdk-10", "Java10"]]);
+    const javaHome: string = process.env.JAVA_HOME;
+    if (!javaHome) {
+        window.showErrorMessage("Please install JDK and set JAVA_HOME in environment variables first!");
+        return;
+    }
+    let matchedValue: string;
+    for (const key of versionFileMapping.keys()) {
+        if (javaHome.match(key) !== null && javaHome.match(key).length !== 0) {
+            matchedValue = javaHome.match(key)[0];
+        }
+    }
+    if (!matchedValue) {
+        window.showErrorMessage("We currently only support JDK (version 1.8.0 or later).");
+        return;
+    }
+    const targetResourceFolder = path.join(extensionPath, "resources", versionFileMapping.get(matchedValue));
+    const location: Uri[] = await window.showOpenDialog({
+        defaultUri: workspace.rootPath ? Uri.file(workspace.rootPath) : undefined,
+        canSelectFiles: false,
+        canSelectFolders: true,
+        openLabel: "Select the location",
+    });
+    if (!location) {
+        return;
+    }
+    const basePath: string = location[0].fsPath;
+    const projectName: string = await window.showInputBox({
+        prompt: "input java project name",
+        validateInput: (name: string): string => {
+            if (name && !name.match(/^[^*~/\\]+$/)) {
+                return "please input a valid project name";
+            }
+            if (name && fse.pathExistsSync(path.join(basePath, name))) {
+                return "A project with this name already exists.";
+            }
+            return null;
+        },
+    });
+    if (!projectName) {
+        return;
+    }
+    const projectFile: string = path.join(basePath, projectName, ".project");
+    await Promise.all([
+        fse.copy(path.join(extensionPath, "resources", "App.java.sample"),
+            path.join(basePath, projectName, "src", "app", "App.java")),
+        fse.copy(path.join(targetResourceFolder, "org.eclipse.jdt.core.prefs"),
+            path.join(basePath, projectName, ".settings", "org.eclipse.jdt.core.prefs")),
+        fse.copy(path.join(targetResourceFolder, ".classpath"),
+            path.join(basePath, projectName, ".classpath")),
+        fse.copy(path.join(extensionPath, "resources", ".project"), projectFile),
+        fse.ensureDir(path.join(basePath, projectName, "bin")),
+    ]);
+
+    // replace the project name with user input project name
+    const xml: string = await fse.readFile(projectFile, "utf8");
+    const jsonObj: any = await parseXml(xml);
+    jsonObj.projectDescription.name = projectName;
+    const builder: xml2js.Builder = new xml2js.Builder();
+    const newXml: string = builder.buildObject(jsonObj);
+    await fse.writeFile(projectFile, newXml);
+
+    commands.executeCommand("vscode.openFolder", Uri.file(path.join(basePath, projectName)), true);
+}
+
+async function parseXml(xml: string): Promise<any> {
+    return new Promise((resolve: (obj: {}) => void, reject: (e: Error) => void): void => {
+        xml2js.parseString(xml, { explicitArray: true }, (err: Error, res: {}) => {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(res);
+        });
+    });
 }
--- vscode/src/views/projectExplorer.ts
+++ vscode/src/views/projectExplorer.ts
@@ -1,90 +1,69 @@
-"use strict";
-
-import * as fse from "fs-extra";
-import * as path from "path";
-import * as vscode from "vscode";
-import * as xml2js from "xml2js";
+import {
+    commands, Event, EventEmitter, ExtensionContext, ProviderResult, Range, Selection,
+    TextEditorRevealType, TreeDataProvider, TreeItem, Uri, window, workspace,
+} from "vscode";
 import { Commands } from "../commands";
-export class ProjectExplorer {
-    private static versionFileMapping: Map<string, string> = new Map([["jdk1.8", "Java8"], ["jdk-9", "Java9"], ["jdk-10", "Java10"]]);
+import { NodeKind } from "../java/nodeData";
+import { ExplorerNode } from "./explorerNode";
+import { WorkspaceNode } from "./workspaceNode";
 
-    constructor(private readonly context: vscode.ExtensionContext) {
+export class ProjectExplorer implements TreeDataProvider<ExplorerNode> {
+
+    private _onDidChangeTreeData: EventEmitter<null> = new EventEmitter<null>();
+
+    // tslint:disable-next-line:member-ordering
+    public onDidChangeTreeData: Event<null> = this._onDidChangeTreeData.event;
+
+    private _rootItems: ExplorerNode[] = null;
+
+    constructor(public readonly context: ExtensionContext) {
+        context.subscriptions.push(commands.registerCommand(Commands.VIEW_PACKAGE_REFRESH, this.refresh, this));
+        context.subscriptions.push(commands.registerCommand(Commands.VIEW_PACKAGE_OPEN_FILE, this.openFile, this));
+        context.subscriptions.push(commands.registerCommand(Commands.VIEW_PACKAGE_OUTLINE, this.goToOutline, this));
     }
 
-    public async createJavaProject() {
-        const javaHome: string = process.env.JAVA_HOME;
-        if (!javaHome) {
-            vscode.window.showErrorMessage("Please install JDK and set JAVA_HOME in environment variables first!");
-            return;
-        }
-        let matchedValue: string;
-        for (const key of ProjectExplorer.versionFileMapping.keys()) {
-            if (javaHome.match(key) !== null && javaHome.match(key).length !== 0) {
-                matchedValue = javaHome.match(key)[0];
-            }
-        }
-        if (!matchedValue) {
-            vscode.window.showErrorMessage("We currently only support JDK (version 1.8.0 or later).");
-            return;
-        }
-        const targetResourceFolder = path.join(this.context.extensionPath, "resources", this._versionFileMapping.get(matchedValue));
-        const location: vscode.Uri[] = await vscode.window.showOpenDialog({
-            defaultUri: vscode.workspace.rootPath ? vscode.Uri.file(vscode.workspace.rootPath) : undefined,
-            canSelectFiles: false,
-            canSelectFolders: true,
-            openLabel: "Select the location",
-        });
-        if (!location) {
-            return;
-        }
-        const basePath: string = location[0].fsPath;
-        const projectName: string = await vscode.window.showInputBox({
-            prompt: "input java project name",
-            validateInput: (name: string): string => {
-                if (name && !name.match(/^[^*~/\\]+$/)) {
-                    return "please input a valid project name";
-                }
-                if (name && fse.pathExistsSync(path.join(basePath, name))) {
-                    return "A project with this name already exists.";
-                }
-                return null;
-            },
-        });
-        if (!projectName) {
-            return;
-        }
-        const projectFile: string = path.join(basePath, projectName, ".project");
-        await Promise.all([
-            fse.copy(path.join(this.context.extensionPath, "resources", "App.java.sample"),
-                path.join(basePath, projectName, "src", "app", "App.java")),
-            fse.copy(path.join(targetResourceFolder, "org.eclipse.jdt.core.prefs"),
-                path.join(basePath, projectName, ".settings", "org.eclipse.jdt.core.prefs")),
-            fse.copy(path.join(targetResourceFolder, ".classpath"),
-                path.join(basePath, projectName, ".classpath")),
-            fse.copy(path.join(this.context.extensionPath, "resources", ".project"), projectFile),
-            fse.ensureDir(path.join(basePath, projectName, "bin")),
-        ]);
-
-        // replace the project name with user input project name
-        const xml: string = await fse.readFile(projectFile, "utf8");
-        const jsonObj: any = await this.parseXml(xml);
-        jsonObj.projectDescription.name = projectName;
-        const builder: xml2js.Builder = new xml2js.Builder();
-        const newXml: string = builder.buildObject(jsonObj);
-        await fse.writeFile(projectFile, newXml);
-
-        vscode.commands.executeCommand("vscode.openFolder", vscode.Uri.file(path.join(basePath, projectName)), true);
+    public refresh() {
+        this._rootItems = null;
+        this._onDidChangeTreeData.fire();
     }
 
-    private async parseXml(xml: string): Promise<any> {
-        return new Promise((resolve: (obj: {}) => void, reject: (e: Error) => void): void => {
-            xml2js.parseString(xml, { explicitArray: true }, (err: Error, res: {}) => {
-                if (err) {
-                    return reject(err);
-                }
-                return resolve(res);
-            });
+    public openFile(uri: string) {
+        return workspace.openTextDocument(Uri.parse(uri)).then((res) => {
+            return window.showTextDocument(res);
         });
     }
 
+    public goToOutline(uri: string, range: Range): Thenable<{}> {
+        return this.openFile(uri).then((editor) => {
+            editor.revealRange(range, TextEditorRevealType.Default);
+            editor.selection = new Selection(range.start, range.start);
+            return commands.executeCommand("workbench.action.focusActiveEditorGroup");
+        });
+    }
+
+    public getTreeItem(element: ExplorerNode): TreeItem | Thenable<TreeItem> {
+        return element.getTreeItem();
+    }
+
+    public getChildren(element?: ExplorerNode): ProviderResult<ExplorerNode[]> {
+        if (!this._rootItems || !element) {
+            this._rootItems = this.getRootNodes();
+            return this._rootItems;
+        } else {
+            return element.getChildren();
+        }
+    }
+
+    private getRootNodes() {
+        const result: ExplorerNode[] = new Array<ExplorerNode>();
+        const folders = workspace.workspaceFolders;
+        if (folders && folders.length) {
+            folders.forEach((folder) => result.push(new WorkspaceNode({
+                name: folder.name,
+                uri: folder.uri.toString(),
+                kind: NodeKind.Workspace,
+            })));
+        }
+        return result;
+    }
 }
