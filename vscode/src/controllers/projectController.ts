950c0727eddb689cfe3e6e5d0d9ce70224a5e9fd
Qingqing Sun <qisun@microsoft.com>
2018-05-04 08:38:27 +0800

    resolve comments

--- vscode/src/controllers/projectController.ts
+++ vscode/src/controllers/projectController.ts
@@ -9,16 +9,8 @@ export class ProjectController {
     }
 
     public async createJavaProject() {
-        let javaVersion: number;
-        try {
-            const javaHome: string = await Utility.checkJavaRuntime();
-            javaVersion = await Utility.checkJavaVersion(javaHome);
-        } catch (error) {
-            window.showErrorMessage(error.message, error.label).then((selection) => {
-                if (error.label && error.label === selection && error.openUrl) {
-                    commands.executeCommand("vscode.open", error.openUrl);
-                }
-            });
+        const javaVersion: number = await this.getJavaVersion();
+        if (!javaVersion) {
             return;
         }
         const location: Uri[] = await window.showOpenDialog({
@@ -46,23 +38,49 @@ export class ProjectController {
         if (!projectName) {
             return;
         }
+        if (await this.scaffoldJavaProject(basePath, projectName, javaVersion)) {
+            commands.executeCommand("vscode.openFolder", Uri.file(path.join(basePath, projectName)), true);
+        }
+    }
+
+    private async scaffoldJavaProject(basePath: string, projectName: string, javaVersion: number): Promise<boolean> {
         const projectFile: string = path.join(basePath, projectName, ".project");
         const extensionPath: string = this.context.extensionPath;
-        await Promise.all([
-            fse.copy(path.join(extensionPath, "templates", "App.java.sample"), path.join(basePath, projectName, "src", "app", "App.java")),
-            fse.copy(path.join(extensionPath, "templates", `Java${javaVersion}`), path.join(basePath, projectName)),
-            fse.copy(path.join(extensionPath, "templates", ".project"), projectFile),
-            fse.ensureDir(path.join(basePath, projectName, "bin")),
-        ]);
+        try {
+            await Promise.all([
+                fse.copy(path.join(extensionPath, "templates", "App.java.sample"), path.join(basePath, projectName, "src", "app", "App.java")),
+                fse.copy(path.join(extensionPath, "templates", `Java${javaVersion}`), path.join(basePath, projectName)),
+                fse.copy(path.join(extensionPath, "templates", ".project"), projectFile),
+                fse.ensureDir(path.join(basePath, projectName, "bin")),
+            ]);
 
-        // replace the project name with user input project name
-        const xml: string = await fse.readFile(projectFile, "utf8");
-        const jsonObj: any = await Utility.parseXml(xml);
-        jsonObj.projectDescription.name = projectName;
-        const builder: xml2js.Builder = new xml2js.Builder();
-        const newXml: string = builder.buildObject(jsonObj);
-        await fse.writeFile(projectFile, newXml);
+            // replace the project name with user input project name
+            const xml: string = await fse.readFile(projectFile, "utf8");
+            const jsonObj: any = await Utility.parseXml(xml);
+            jsonObj.projectDescription.name = projectName;
+            const builder: xml2js.Builder = new xml2js.Builder();
+            const newXml: string = builder.buildObject(jsonObj);
+            await fse.writeFile(projectFile, newXml);
+        } catch (error) {
+            window.showErrorMessage(error.message);
+            return;
+        }
+        return true;
+    }
 
-        commands.executeCommand("vscode.openFolder", Uri.file(path.join(basePath, projectName)), true);
+    private async getJavaVersion(): Promise<number> {
+        let javaVersion: number;
+        try {
+            const javaHome: string = await Utility.checkJavaRuntime();
+            javaVersion = await Utility.checkJavaVersion(javaHome);
+        } catch (error) {
+            window.showErrorMessage(error.message, error.label).then((selection) => {
+                if (error.label && error.label === selection && error.openUrl) {
+                    commands.executeCommand("vscode.open", error.openUrl);
+                }
+            });
+            return;
+        }
+        return javaVersion;
     }
 }
