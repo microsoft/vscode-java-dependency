82a2375e9e89b2c00f779f2dfb4caa00e5047043
Yaohai Zheng <yaozheng@microsoft.com>
2018-07-30 15:06:39 +0800

    Refine java dependency viewer configuration.

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,18 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
-import { commands, ExtensionContext, window } from "vscode";
+import { commands, ExtensionContext } from "vscode";
 import { Commands } from "./commands";
 import { ProjectController } from "./controllers/projectController";
 import { Services } from "./services";
+import { Settings } from "./settings";
 import { Telemetry } from "./telemetry";
 import { DependencyExplorer } from "./views/depedencyExplorer";
 
 export function activate(context: ExtensionContext) {
     Telemetry.sendEvent("activateExtension", {});
     Services.initialize(context);
+    Settings.initialize(context);
 
     const projectController: ProjectController = new ProjectController(context);
     context.subscriptions.push(commands.registerCommand(Commands.JAVA_PROJECT_CREATE, async () => { projectController.createJavaProject(); }));
--- src/settings.ts
+++ src/settings.ts
@@ -1,12 +1,32 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
-import { workspace, WorkspaceConfiguration } from "vscode";
+import { commands, ConfigurationChangeEvent, ExtensionContext, workspace, WorkspaceConfiguration } from "vscode";
+import { Commands } from "./commands";
 
 export class Settings {
-    public static showOutline(): boolean {
-        return this._debugSettingsRoot.get("showOutline");
+
+    public static initialize(context: ExtensionContext): void {
+        context.subscriptions.push(workspace.onDidChangeConfiguration((e: ConfigurationChangeEvent) => {
+            if (!e.affectsConfiguration("java.dependency")) {
+                return;
+            }
+            const updatedConfig = workspace.getConfiguration("java.dependency");
+            if (updatedConfig.showOutline !== this._depdendencyConfig.showOutline) {
+                commands.executeCommand(Commands.VIEW_PACKAGE_REFRESH);
+            }
+            this._depdendencyConfig = updatedConfig;
+
+        }));
     }
 
-    private static _debugSettingsRoot: WorkspaceConfiguration = workspace.getConfiguration("java.dependency");
+    public static showOutline(): boolean {
+        return this._depdendencyConfig.get("showOutline");
+    }
+
+    public static syncWithFolderExplorer(): boolean {
+        return this._depdendencyConfig.get("syncWithFolderExplorer");
+    }
+
+    private static _depdendencyConfig: WorkspaceConfiguration = workspace.getConfiguration("java.dependency");
 }
