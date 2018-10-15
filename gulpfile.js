a4fc600b469d2cc2c48abc88c42749f355e11d14
andxu <andxu@microsoft.com>
2018-10-15 10:03:25 +0800

    1. revert nodejs version since latest version is not used by vscode. 2. update gulp to version 4

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ gulp.task('build_server', ()=>
 {
   cp.execSync(mvnw()+ ' clean package', {cwd:server_dir, stdio:[0,1,2]} );
   
-  gulp.src(server_dir + '/com.microsoft.jdtls.ext.core/target/com.microsoft.jdtls.ext.core*.jar')
+  return gulp.src(server_dir + '/com.microsoft.jdtls.ext.core/target/com.microsoft.jdtls.ext.core*.jar')
 		.pipe(gulp.dest('./server'))
 });
 
