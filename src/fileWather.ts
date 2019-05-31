c9d6f136f7eb94dcf4cb72860c11ea1a6a141574
Yaohai Zheng <yaozheng@microsoft.com>
2019-05-31 10:37:09 +0800

    Make tslint happy. (#157)

--- src/fileWather.ts
+++ src/fileWather.ts
@@ -1,23 +1,19 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
-import { Disposable, workspace, commands, FileSystemWatcher } from "vscode";
+import { commands, Disposable, FileSystemWatcher, workspace } from "vscode";
 import { Commands } from "./commands";
 
 export class SyncHandler {
 
-    private static javaFileContentWatcher: Disposable = null;
-
-    private static javaFileSystemWatcher: FileSystemWatcher = null;
-
     public static updateFileWatcher(autoRefresh: boolean): void {
         if (autoRefresh) {
             SyncHandler.javaFileContentWatcher = workspace.onDidChangeTextDocument((event) => {
-                if (event.document.languageId === 'java') {
+                if (event.document.languageId === "java") {
                     SyncHandler.refresh();
                 }
             });
-            SyncHandler.javaFileSystemWatcher = workspace.createFileSystemWatcher('**/*.{java}');
+            SyncHandler.javaFileSystemWatcher = workspace.createFileSystemWatcher("**/*.{java}");
             SyncHandler.javaFileSystemWatcher.onDidChange(SyncHandler.refresh);
             SyncHandler.javaFileSystemWatcher.onDidCreate(SyncHandler.refresh);
             SyncHandler.javaFileSystemWatcher.onDidDelete(SyncHandler.refresh);
@@ -30,6 +26,10 @@ export class SyncHandler {
             }
         }
     }
+
+    private static javaFileContentWatcher: Disposable = null;
+
+    private static javaFileSystemWatcher: FileSystemWatcher = null;
 
     private static refresh(): void {
         commands.executeCommand(Commands.VIEW_PACKAGE_REFRESH);
