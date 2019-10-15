90734a7f843d6a85aeffd4d8306ac2ca3f3ede5b
Vigilans <vigilans@foxmail.com>
2019-10-15 15:18:26 +0800

    Resolve redhat.java dependency

--- test/index.ts
+++ test/index.ts
@@ -1,11 +1,21 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
+import * as cp from "child_process";
 import * as path from "path";
-import { runTests } from "vscode-test";
+import { downloadAndUnzipVSCode, resolveCliPathFromVSCodeExecutablePath, runTests } from "vscode-test";
 
 async function main(): Promise<void> {
     try {
+        const vscodeExecutablePath = await downloadAndUnzipVSCode();
+        const cliPath = resolveCliPathFromVSCodeExecutablePath(vscodeExecutablePath);
+
+        // Resolve redhat.java dependency
+        cp.spawnSync(cliPath, ["--install-extension", "redhat.java"], {
+            encoding: "utf-8",
+            stdio: "inherit",
+        });
+
         // The folder containing the Extension Manifest package.json
         // Passed to `--extensionDevelopmentPath`
         const extensionDevelopmentPath: string = path.resolve(__dirname, "../../");
@@ -15,7 +25,7 @@ async function main(): Promise<void> {
         const extensionTestsPath: string = path.resolve(__dirname, "./suite/index");
 
         // Download VS Code, unzip it and run the integration test
-        await runTests({ extensionDevelopmentPath, extensionTestsPath });
+        await runTests({ vscodeExecutablePath, extensionDevelopmentPath, extensionTestsPath });
     } catch (err) {
         process.exit(1);
     }
