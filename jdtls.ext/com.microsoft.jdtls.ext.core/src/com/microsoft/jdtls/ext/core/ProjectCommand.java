1f3339de0dd32aefc18e19875982c70be233eb2a
Sheng Chen <sheche@microsoft.com>
2019-11-12 14:53:27 +0800

    Use workspace folder name as the invisible project name

--- jdtls.ext/com.microsoft.jdtls.ext.core/src/com/microsoft/jdtls/ext/core/ProjectCommand.java
+++ jdtls.ext/com.microsoft.jdtls.ext.core/src/com/microsoft/jdtls/ext/core/ProjectCommand.java
@@ -16,11 +16,13 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
+import org.apache.commons.io.FilenameUtils;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jdt.ls.core.internal.ProjectUtils;
 import org.eclipse.jdt.ls.core.internal.ResourceUtils;
 
 import com.microsoft.jdtls.ext.core.model.NodeKind;
@@ -31,17 +33,22 @@ public final class ProjectCommand {
     public static List<PackageNode> execute(List<Object> arguments, IProgressMonitor monitor) {
         String workspaceUri = (String) arguments.get(0);
         IPath workspacePath = ResourceUtils.canonicalFilePathFromURI(workspaceUri);
-        String invisibleProjectName = getWorkspaceInvisibleProjectName(workspacePath);
 
         IProject[] projects = getWorkspaceRoot().getProjects();
         ArrayList<PackageNode> children = new ArrayList<>();
         List<IPath> paths = Arrays.asList(workspacePath);
         for (IProject project : projects) {
-            if (project.exists() && (ResourceUtils.isContainedIn(project.getLocation(), paths) || Objects.equals(project.getName(), invisibleProjectName))) {
-                PackageNode projectNode = new PackageNode(project.getName(), project.getFullPath().toPortableString(), NodeKind.PROJECT);
-                projectNode.setUri(project.getLocationURI().toString());
-                children.add(projectNode);
+            String projectName = project.getName();
+            if (Objects.equals(projectName, ProjectUtils.getWorkspaceInvisibleProjectName(workspacePath))) {
+                // Should not use internal project name for invisible project
+                projectName = FilenameUtils.getBaseName(workspaceUri);
+            } else if (!project.exists() || (!ResourceUtils.isContainedIn(project.getLocation(), paths))) {
+                continue;
             }
+
+            PackageNode projectNode = new PackageNode(projectName, project.getFullPath().toPortableString(), NodeKind.PROJECT);
+            projectNode.setUri(project.getLocationURI().toString());
+            children.add(projectNode);
         }
 
         return children;
@@ -49,11 +56,5 @@ public final class ProjectCommand {
 
     private static IWorkspaceRoot getWorkspaceRoot() {
         return ResourcesPlugin.getWorkspace().getRoot();
-    }
-
-    // TODO Use ProjectUtils.getWorkspaceInvisibleProjectName directly when the language server is released.
-    private static String getWorkspaceInvisibleProjectName(IPath workspacePath) {
-        String fileName = workspacePath.toFile().getName();
-        return fileName + "_" + Integer.toHexString(workspacePath.toPortableString().hashCode());
     }
 }
