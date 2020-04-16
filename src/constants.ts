7cca3bb37b6fb58361f54bd82dc717e51aa34bd8
Sheng Chen <sheche@microsoft.com>
2020-04-16 16:26:53 +0800

    Improve the create project command
    Signed-off-by: Sheng Chen <sheche@microsoft.com>

---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+export namespace Context {
+    export const MAVEN_ENABLED: string = "mavenEnabled";
+    export const EXTENSION_ACTIVATED: string = "extensionActivated";
+}
--- templates/invisible-project/src/App.java
+++ templates/invisible-project/src/App.java
@@ -1,7 +1,5 @@
-package app;
-
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        System.out.println("Hello, World!");
     }
 }
