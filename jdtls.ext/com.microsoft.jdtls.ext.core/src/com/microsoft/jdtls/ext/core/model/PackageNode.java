ea43694cdfe264f42917c99d1637a939c5e49cd3
Sheng Chen <sheche@microsoft.com>
2020-08-25 11:20:07 +0800

    Use folder name as the name of the invisible project (#312)

--- jdtls.ext/com.microsoft.jdtls.ext.core/src/com/microsoft/jdtls/ext/core/model/PackageNode.java
+++ jdtls.ext/com.microsoft.jdtls.ext.core/src/com/microsoft/jdtls/ext/core/model/PackageNode.java
@@ -74,6 +74,11 @@ public class PackageNode {
     private String name;
 
     /**
+     * The display name of the node
+     */
+    private String displayName;
+
+    /**
      * The module name of the PackageNode for Java 9 and above
      */
     private String moduleName;
@@ -273,6 +278,10 @@ public class PackageNode {
 
     public String getName() {
         return name;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
     }
 
     public void setModuleName(String moduleName) {
