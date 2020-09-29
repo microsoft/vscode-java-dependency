17f79e9c5c7451992a5d8c1b384adc6343660bfb
xqzlgy2 <xqzlgy2@gmail.com>
2020-09-29 13:39:36 +0800

    Change message prompt to modal

--- src/explorerCommands/delete.ts
+++ src/explorerCommands/delete.ts
@@ -4,19 +4,11 @@
 import { window, workspace,  Uri } from "vscode";
 import { DataNode } from "../views/dataNode";
 
-const deleteOptions = {
-    recursive: true,
-    useTrash: true
-}
-
-const userActions = {
-    confirm: "Move to Recycle Bin",
-    cancel: "Cancel"
-}
+const confirmMessage = "Move to Recycle Bin";
 
 function getInformationMessage(name: string, isFolder: boolean): string {
     const folderMsg = isFolder ? " and its contents" : "";
-    const msg = `Are you sure you want to delete \'${name}\'${folderMsg}? `;
+    const msg = `Are you sure you want to delete \'${name}\'${folderMsg}?\n`;
     const additionMsg = "You can restore from the Recycle Bin.";
     return msg + additionMsg;
 }
@@ -25,15 +17,21 @@ export async function deleteFiles(node: DataNode): Promise<void> {
     const children = await node.getChildren();
     const isFolder = children && children.length !== 0;
     const message = getInformationMessage(node.name, isFolder);
-    const actions = [userActions.confirm, userActions.cancel];
 
-    let answer: string | undefined = await window.showInformationMessage(message, ...actions);
+    let answer: string | undefined = await window.showInformationMessage(
+        message, 
+        { modal: true },
+        confirmMessage
+    );
 
     if (!answer) {
         return;
     }
     
-    if (answer == userActions.confirm) {
-        workspace.fs.delete(Uri.parse(node.uri), deleteOptions);
+    if (answer == confirmMessage) {
+        workspace.fs.delete(Uri.parse(node.uri), {
+            recursive: true,
+            useTrash: true
+        });
     }
 }
