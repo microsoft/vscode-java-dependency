54911c34dd08ca258bfa6e72f2563dd01293b315
Sheng Chen <sheche@microsoft.com>
2021-02-09 09:31:55 +0800

    fix: Cannot reveal the explorer when 'java.dependency.syncWithFolderExplorer' is off (#460)

--- src/utility.ts
+++ src/utility.ts
@@ -5,7 +5,6 @@ import { Uri, window, workspace, WorkspaceFolder } from "vscode";
 import { setUserError } from "vscode-extension-telemetry-wrapper";
 import { INodeData } from "./java/nodeData";
 import { languageServerApiManager } from "./languageServerApi/languageServerApiManager";
-import { Settings } from "./settings";
 
 export class Utility {
 
@@ -31,7 +30,7 @@ export class Utility {
         if (uri.scheme === "file" && !workspace.getWorkspaceFolder(uri)) {
             return false;
         }
-        if (!Settings.syncWithFolderExplorer() || !await languageServerApiManager.isStandardServerReady()) {
+        if (!await languageServerApiManager.isStandardServerReady()) {
             return false;
         }
         return true;
