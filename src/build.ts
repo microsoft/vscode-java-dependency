e4187e29c82e18397838520b894b3595213093c3
Shi Chen <chenshi@microsoft.com>
2021-03-08 16:03:53 +0800

    accept error type

--- src/build.ts
+++ src/build.ts
@@ -36,7 +36,10 @@ async function handleBuildFailure(operationId: string, err: any): Promise<boolea
     });
     setErrorCode(error, Number(err));
     sendOperationError(operationId, "build", error);
-    if (err === Jdtls.CompileWorkspaceStatus.Witherror || err === Jdtls.CompileWorkspaceStatus.Failed) {
+    // workaround: Since VS Code 1.53, the contributed command would no longer throw exact error message when an error occurs.
+    // This change breaks the existing build error reporting, so we make a workaround here.
+    // Related issue: https://github.com/microsoft/vscode/issues/116932
+    if (err instanceof Error || err === Jdtls.CompileWorkspaceStatus.Witherror || err === Jdtls.CompileWorkspaceStatus.Failed) {
         if (checkErrorsReportedByJavaExtension()) {
             commands.executeCommand("workbench.actions.view.problems");
         }
