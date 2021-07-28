2f1500fee174c5d0585346eb40e719b08d351417
Shi Chen <chenshi@microsoft.com>
2021-07-28 14:42:32 +0800

    fix lint

--- test/ui/index.ts
+++ test/ui/index.ts
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
-import * as path from "path";
 import * as os from "os";
+import * as path from "path";
 import { ExTester } from "vscode-extension-tester";
 
 async function main(): Promise<void> {
