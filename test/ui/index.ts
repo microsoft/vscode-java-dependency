fd3fb4ec5bbffe8cf82ef968e45d9d26ebb32fb6
Shi Chen <chenshi@microsoft.com>
2021-08-11 09:56:42 +0800

    address comment

--- test/ui/index.ts
+++ test/ui/index.ts
@@ -2,17 +2,21 @@
 // Licensed under the MIT license.
 
 import * as fse from "fs-extra";
-import * as os from "os";
 import * as path from "path";
 import * as semver from "semver";
 import { ExTester } from "vscode-extension-tester";
 
+/* tslint:disable:no-console */
 async function main(): Promise<void> {
     try {
         // Run UI command tests
         const packageContent = await fse.readFile(path.join(__dirname, "..", "..", "..", "package.json"));
         const packageJSON = JSON.parse(packageContent.toString());
-        let vscodeVersion = packageJSON.engines.vscode || "1.59.0";
+        let vscodeVersion = packageJSON.engines.vscode;
+        if (!vscodeVersion) {
+            console.log("No valid version of VSCode engine was found in package.json");
+            process.exit(1);
+        }
         vscodeVersion = semver.minVersion(vscodeVersion);
         const version = vscodeVersion.version;
         const testPath = path.join(__dirname, "command.test.js");
@@ -24,7 +28,7 @@ async function main(): Promise<void> {
         await exTester.setupRequirements({vscodeVersion: version});
         process.exit(await exTester.runTests(testPath, {vscodeVersion: version}));
     } catch (err) {
-        process.stdout.write(`${err}${os.EOL}`);
+        console.log(err);
         process.exit(1);
     }
 }
