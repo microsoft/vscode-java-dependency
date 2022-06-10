076edb26ab3ec2f7570041e78f67803b98aaf641
sheche <sheche@microsoft.com>
2022-06-10 10:52:55 +0800

    feat: Support drag and drop to editor
    - Primary type nodes and file nodes can drag to editor.
    - Document symbol nodes can drag to editor with its range.
    
    Signed-off-by: sheche <sheche@microsoft.com>

---
+++ src/views/DragAndDropController.ts
@@ -0,0 +1,41 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+import { DataTransfer, DataTransferItem, TreeDragAndDropController, Uri } from "vscode";
+import { Explorer } from "../constants";
+import { BaseSymbolNode } from "./baseSymbolNode";
+import { DataNode } from "./dataNode";
+import { ExplorerNode } from "./explorerNode";
+import { FileNode } from "./fileNode";
+import { PrimaryTypeNode } from "./PrimaryTypeNode";
+import { SymbolNode } from "./symbolNode";
+
+export class DragAndDropController implements TreeDragAndDropController<ExplorerNode> {
+
+    dropMimeTypes: string[] = [
+        Explorer.Mime.JavaProjectExplorer,
+    ];
+    dragMimeTypes: string[] = [
+        Explorer.Mime.TextUriList,
+    ];;
+
+    public handleDrag(source: ExplorerNode[], treeDataTransfer: DataTransfer): void {
+        // select many is not supported yet
+        let dragItem = source[0];
+        this.addDragToEditorDataTransfer(dragItem, treeDataTransfer);
+    }
+
+    private addDragToEditorDataTransfer(node: ExplorerNode, treeDataTransfer: DataTransfer) {
+        if ((node instanceof PrimaryTypeNode || node instanceof FileNode) && (node as DataNode).uri) {
+            treeDataTransfer.set(Explorer.Mime.TextUriList, new DataTransferItem((node as DataNode).uri));
+        } else if ((node instanceof BaseSymbolNode)) {
+            const parent = (node.getParent() as PrimaryTypeNode);
+            if (parent.uri) {
+                const range = (node as BaseSymbolNode).range;
+                const fragment = `#L${range.start.line + 1},${range.start.character + 1}`;
+                const uri = parent.uri + fragment;
+                treeDataTransfer.set(Explorer.Mime.TextUriList, new DataTransferItem(uri));
+            }
+        }
+    }
+}
