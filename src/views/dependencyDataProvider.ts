e9f8b3fdde6e6566096c7e980fa5f34343bbf11b
Thomas Mäder <t.s.maeder@gmail.com>
2025-11-18 17:22:31 +0100

    Always show Java projects even when errors occur during import
    When resolving dependencies for Java projects, errors can sometimes occur due to
    
    Signed-off-by: Thomas Mäder <t.s.maeder@gmail.com>

--- src/views/dependencyDataProvider.ts
+++ src/views/dependencyDataProvider.ts
@@ -184,7 +184,6 @@ export class DependencyDataProvider implements TreeDataProvider<ExplorerNode> {
             const hasJavaError: boolean = await Jdtls.checkImportStatus();
             if (hasJavaError) {
                 contextManager.setContextValue(Context.IMPORT_FAILED, true);
-                return [];
             }
 
             const rootItems: ExplorerNode[] = [];
